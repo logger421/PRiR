e4f596439666dcbd10ffa532d340f3126e0fde7a
Logger421 <wojteklotko3@gmail.com>
2023-11-30 21:37:09 +0100

    Added solution class

---
+++ exc/ex2/src/ParallelEmployer.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class ParallelEmployer implements Employer {
+    @Override
+    public void setOrderInterface(OrderInterface order) {
+
+    }
+
+    @Override
+    public Location findExit(Location startLocation, List<Direction> allowedDirections) {
+        return null;
+    }
+}
